29034d48aa54f9a6a84ce928ec5101615eacbf06
ShrBox <shrbox@outlook.com>
2020-05-08 19:16:43 +0800

    optimization

--- src/main/java/top/mc114/groupadmin/GroupMemberRequest.java
+++ src/main/java/top/mc114/groupadmin/GroupMemberRequest.java
@@ -2,16 +2,15 @@ package top.mc114.groupadmin;
 
 import net.mamoe.mirai.event.events.MemberJoinRequestEvent;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
 public class GroupMemberRequest implements Consumer <MemberJoinRequestEvent> {
-    List<String> list = new ArrayList<>();
+    List<String> list;
     @Override
     public void accept(MemberJoinRequestEvent event) {
         list = BotMain.accept_list;
-        for(int a = 0;a < list.size();a++) {
+        for(int a=0;a<list.size();a++) {
             if(event.getMessage().toLowerCase().contains(list.get(a))) {
                 event.accept();
                 event.getGroup().sendMessage(event.getFromNick()+"["+event.getFromId()+"]经过Bot的审核加入了本群。");
--- src/main/java/top/mc114/groupadmin/GroupMessageListener.java
+++ src/main/java/top/mc114/groupadmin/GroupMessageListener.java
@@ -4,22 +4,18 @@ import net.mamoe.mirai.message.GroupMessage;
 import net.mamoe.mirai.message.data.At;
 import net.mamoe.mirai.message.data.MessageUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
 public class GroupMessageListener implements Consumer <GroupMessage> {
-    List<String> list = new ArrayList<>();
+    List<String> list;
     @Override
     public void accept(GroupMessage event) {
-        //System.out.println(event.getGroup().getBotPermission().getLevel());//1=Administrator,0=Member
+        //1=Administrator,0=Member
         if(event.getGroup().getBotPermission().getLevel()==0||event.getSender().getPermission().getLevel()>0) {
-            //System.out.println("Permission denied!");
             return;
         }
-        String msg = event.getMessage().toString();
         list = BotMain.key_list;
-        //double st = System.currentTimeMillis();
         for(int a=0;a<list.size();a++) {
             if(event.getMessage().toString().toLowerCase().contains(list.get(a))) {
                 event.getBot().recall(event.getMessage());
@@ -27,8 +23,6 @@ public class GroupMessageListener implements Consumer <GroupMessage> {
                 event.getSender().muteAsync(time);
                 event.getGroup().sendMessage(MessageUtils.newChain(new At(event.getSender()))
                         .plus("你使用了违禁词，你因此被禁言"+time+"秒，请注意自己的言行！"));
-                //double et = System.currentTimeMillis();
-                //System.out.println("操作耗时:"+(et-st));
                 return;
             }
         }
