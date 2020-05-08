31b1984954ba11c26cc3bd1ec18408133df824e3
ShrBox <shrbox@outlook.com>
2020-05-08 17:42:06 +0800

    optimization

--- src/main/java/top/mc114/groupadmin/BotMain.java
+++ src/main/java/top/mc114/groupadmin/BotMain.java
@@ -1,18 +1,25 @@
 package top.mc114.groupadmin;
 
+import net.mamoe.mirai.console.plugins.Config;
 import net.mamoe.mirai.console.plugins.PluginBase;
 import net.mamoe.mirai.event.events.MemberJoinEvent;
 import net.mamoe.mirai.event.events.MemberJoinRequestEvent;
 import net.mamoe.mirai.event.events.MemberLeaveEvent;
 import net.mamoe.mirai.message.GroupMessage;
 
-class BotMain extends PluginBase {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BotMain extends PluginBase {
+    public static List<String> list = new ArrayList<>();
+
     public void onLoad() {
-        getLogger().info("GroupAdministrator plugin loaded!");
+        Collections.addAll(list,"ddos","傻逼","翻墙","vpn",
+                "习近平","ssr","v2ray","社工","人肉","梯子","脑瘫","你妈");
     }
 
     public void onEnable() {
-        getLogger().info("GroupAdministrator plugin enabled! Version:0.1.1");
         this.getEventListener().subscribeAlways(GroupMessage.class, new GroupMessageListener());
         this.getEventListener().subscribeAlways(MemberJoinEvent.class, new GroupMemberJoinListener());
         this.getEventListener().subscribeAlways(MemberLeaveEvent.class, new GroupMemberLeaveListener());
--- src/main/java/top/mc114/groupadmin/GroupMessageListener.java
+++ src/main/java/top/mc114/groupadmin/GroupMessageListener.java
@@ -1,21 +1,16 @@
 package top.mc114.groupadmin;
 
-import kotlinx.coroutines.Dispatchers;
-import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.message.GroupMessage;
 import net.mamoe.mirai.message.data.At;
-import net.mamoe.mirai.message.data.Image;
 import net.mamoe.mirai.message.data.MessageUtils;
 
-import javax.naming.ldap.Control;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 
 public class GroupMessageListener implements Consumer <GroupMessage> {
+    List<String> list = new ArrayList<>();
     @Override
     public void accept(GroupMessage event) {
         //System.out.println(event.getGroup().getBotPermission().getLevel());//1=Administrator,0=Member
@@ -24,9 +19,8 @@ public class GroupMessageListener implements Consumer <GroupMessage> {
             return;
         }
         String msg = event.getMessage().toString();
-        List<String> list = new ArrayList<>();
-        Collections.addAll(list,"ddos","傻逼","翻墙","vpn",
-                "习近平","ssr","v2ray","社工","人肉","梯子","脑瘫","你妈");
+        list = BotMain.list;
+        //double st = System.currentTimeMillis();
         for(int a=0;a<list.size();a++) {
             if(event.getMessage().toString().toLowerCase().contains(list.get(a))) {
                 event.getBot().recall(event.getMessage());
@@ -34,6 +28,8 @@ public class GroupMessageListener implements Consumer <GroupMessage> {
                 event.getSender().muteAsync(time);
                 event.getGroup().sendMessage(MessageUtils.newChain(new At(event.getSender()))
                         .plus("你使用了违禁词，你因此被禁言"+time+"秒，请注意自己的言行！"));
+                //double et = System.currentTimeMillis();
+                //System.out.println("操作耗时:"+(et-st));
                 return;
             }
         }
