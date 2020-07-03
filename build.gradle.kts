b0ee29551de7a31a54217bbd181bee5ba5e7db8c
ShrBox <shrbox@outlook.com>
2020-07-03 22:30:20 +0800

    build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 val miraiCoreVersion = "+" // 1.0-RC
-val miraiConsoleVersion = "+" // 0.5.1
+val miraiConsoleVersion = "0.5.2" // 0.5.1
 
 dependencies {
     compileOnly(kotlin("stdlib-jdk8"))
